6894f7ee78f8d262a7a5ef581a45ce9e16a22a80
Yuugen64 <yuugen64@protonmail.com>
2021-05-02 22:38:28 -0500

    Added vertex and fragment shader, compiled, added to shaderProgram.

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+//Vertex Shader
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+//Fragment Shader
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
 int main()
 {
 	//Initialize glfw so we can use its functions.
@@ -14,8 +30,48 @@ int main()
 	//an openGL profile is a set of functions. 2 types, 'core' (modern funcs), and 'compatability' (modern + older funcs).
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	//Coordinates for a vertex shader
+	//We use openGL-float datatype because normal floats may differ in size causing errors.
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3.0f,
+		-0.5f, -0.5f * float(sqrt(3)) / 3.0f,
+		0.0f, 0.5 * float(sqrt(3)) * 2 / 3, 0.0f
+	};
+
+
+
+
+
 	//glfw window type
 	GLFWwindow* window = glfwCreateWindow(800, 800, "FreeCodeCampOpenGL", NULL, NULL);
+
+	//Attempting to create our vertex shader by referencing an openGL vertex shader
+	//GLuint -> openGL unsigned integer, i.e. a positive integer using all 8 bits for values.
+	//glCreateShader() is the call, GL_VERTEX_SHADER is the type of shader.
+	GLuint myVertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(myVertexShader, 1, &vertexShaderSource, NULL);
+	//The graphics card can't understand our shader code however, so it must be compiled into MACHINE CODE right now.
+	glCompileShader(myVertexShader);
+
+	//Repeat for fragment shader
+	GLuint myFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(myFragmentShader, 1, &fragmentShaderSource, NULL);
+	//The graphics card can't understand our shader code however, so it must be compiled into MACHINE CODE right now.
+	glCompileShader(myFragmentShader);
+
+	//In order to use the shaders we need to "wrap" them into a shader program.
+	GLuint shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, myVertexShader);
+	glAttachShader(shaderProgram, myFragmentShader);
+	glLineWidth(shaderProgram);
+
+	//We can delete our previous shaders because they're already in our shaderProgram.
+	glDeleteShader(myVertexShader);
+	glDeleteShader(myFragmentShader);
+
+
 
 	//Error checking in case window fails to create.
 	if (window == NULL) 
